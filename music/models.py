eb4a9a68c797e8d57839931d012c100ae58d9e8a
YuTao <yutao1016@gmail.com>
2016-11-29 19:21:00 +0800

    NO.25

--- music/models.py
+++ music/models.py
@@ -14,7 +14,7 @@ class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	song_title = models.CharField(max_length=100)
 	file_type = models.CharField(max_length=10, default='mp3')
-	song_logo = models.CharField(max_length=1000) 
+	is_favorite = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.song_title 
--- website/urls.py
+++ website/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
 ]
+                                               
